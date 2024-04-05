19b61a643efc27c35544e67a66168018806ab57d
dam-51 <dam51@hotmail.fr>
2024-04-05 12:12:52 +0200

    terminé

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//  ()=> {}  fonction anonyme car pas de nom
+// function truc = (){} fonction non anonyme car elle a un nom
+
 //on crée une fonction qui génère un chiffre entre 0 et 255
 function randomNumber(){
     return Math.floor(Math.random() * 256);     //floor arrondi la valeur à l'entier en dessous
@@ -16,9 +19,15 @@ function checkDiv(){
     if (boxes.length > 0) {
         document.getElementById('removeLastButton').style.display = 'inline-block';        //block pour qu'ils apparaissent tout simplement
         document.getElementById('removeAllDiv').style.display = 'inline-block';
+        if (boxes.length > 1){
+            document.getElementById('shuffleButton').style.display = 'inline-block';
+        }else{
+            document.getElementById('shuffleButton').style.display = 'none';
+        }
     } else {
         document.getElementById('removeLastButton').style.display = 'none';
         document.getElementById('removeAllDiv').style.display = 'none';
+        document.getElementById('shuffleButton').style.display = 'none';
     }
 }
 
@@ -75,6 +84,24 @@ document.getElementById('removeAllDiv').addEventListener("click", ()=>{
     removeAllDiv();
 })
 
+//fonction qui mélange le tableau de div
+function shuffleDiv(){
+    let container = document.getElementById('container');
+    let boxes = document.getElementsByClassName('box');
+    if(boxes.length > 1){
+        //on mélange les éléments du tableau divArray
+        let divArray = Array.from(boxes).sort(() => Math.random() - 0.5);
+        //on vide le container
+        container.innerHTML ="";
+        //on ajoute les divs dans le container
+        divArray.forEach((div) => {
+            container.appendChild(div);
+        });
+        
+    }
+}
+
+
 
 //on jaoute un évènement à notre bouton aved l'id "addButton"
 //je récupère le bouton avec l'id button
@@ -92,6 +119,10 @@ document.getElementById('removeLastButton').addEventListener("click", () =>{
     removeLastDiv();
 })
 
+//on ajoute un évènement à notre bouton avec l'id "shuffleButton"
+document.getElementById('shuffleButton').addEventListener("click", () =>{
+    shuffleDiv();
+})
 
 //appel initial de la fonction checkDiv au démarrage de l'appli
 checkDiv();
